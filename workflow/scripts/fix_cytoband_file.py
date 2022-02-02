9c70627dac4ff4b75e6eb777eeea418e4976eb20
Richard de Borja <richard.deborja@gmail.com>
2022-02-01 22:26:25 -0500

    script to append cytoband file with column joining chr and cytoband

---
+++ workflow/scripts/fix_cytoband_file.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+Append a column to the UCSC cytoband file
+"""
+
+import os
+import sys
+import argparse
+import csv
+
+
+def init_args():
+    """
+    Initialize command line arguments
+    """
+    description = 'Append chromosome identifier column to UCSC cytoband file'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-f', '--file', required=True,
+        help='full path to UCSC cytoband file')
+    return parser.parse_args()
+
+
+def main():
+    """
+    Main program
+    """
+    args = init_args()
+    fieldnames = ['chrom', 'start', 'end', 'band', 'desc']
+    with open(args.file, 'r') as ifh:
+        reader = csv.DictReader(ifh, delimiter='\t', fieldnames=fieldnames)
+        for line in reader:
+            chr_cyto = ''.join(['chr', line['band']])
+            print('\t'.join([
+                line['chrom'],
+                line['start'],
+                line['end'],
+                line['band'],
+                line['desc'],
+                chr_cyto
+            ]))
+    ifh.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
+#__END__
